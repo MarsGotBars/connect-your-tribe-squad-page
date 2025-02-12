88b3e3764d0c8dfebda0e31fd3d9cb0443d56235
MarsGotBars <marcinm@live.nl>
2025-02-12 01:44:59 +0100

    stashin
    see you later

---
+++ public/script/script.js
@@ -0,0 +1,12 @@
+const people = document.querySelectorAll('li');
+const peopleContainer = document.querySelector('ul');
+
+const randomPlacement = () => {
+    people.forEach(person => {
+        person.style.top = 
+    });
+}
+
+const topPlacement = (min, max) =>{
+    return 
+}
